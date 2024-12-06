6e8e4247fb67beef2623829cbbce9d7cb3002a52
Selam Beyene <SELAMBEYENE2@GMAIL.COM>
2024-12-06 01:29:58 +0000

    completes registerAccount functionality

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.exception.AccountAlreadyExistsException;
+import com.example.exception.InvalidPasswordException;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,46 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+ @RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> registerUser(@RequestBody Account acct){
+
+        try {
+            Account newAccount = accountService.registerAccount(acct);
+            return ResponseEntity.status(200).body(newAccount);
+
+        } catch (AccountAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+
+        } catch (InvalidPasswordException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
 }
+
+
+
+/**
+ *     @GetMapping("/string/{text}")
+    public String getStringPathVariable(@PathVariable String text){
+        return text;
+    }
+ * 
+ *      
+@PostMapping(value = "/requestbody") <- can omit 'value = '
+public Sample postSample(@RequestBody Sample pojo){
+    //you will need to change the method's parameters and return the extracted request body.
+    return pojo;
+}
+
+@GetMapping("/exampleHeaders")
+public ResponseEntity headers(@RequestBody Sample sample){
+    return ResponseEntity.status(200).header("content-type", "application/zip").body(sample);
+}
+ */
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,33 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.exception.AccountAlreadyExistsException;
+import com.example.exception.InvalidPasswordException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account registerAccount(Account account){
+        Optional<Account> existingAccount = accountRepository.findByUsername(account.getUsername());
+
+        if(existingAccount.isPresent()) {
+            throw new AccountAlreadyExistsException();
+        }
+
+        if(account.getPassword().length() < 4){
+            throw new InvalidPasswordException();
+        }
+
+        return accountRepository.save(account);
+    }
+
 }
