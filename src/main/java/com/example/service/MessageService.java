63a20ccf575fa9397c51c107b77e06fd629c740e
Selam Beyene <SELAMBEYENE2@GMAIL.COM>
2024-12-06 02:26:38 +0000

    completes getAllMessages functionality

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,21 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
+import com.example.entity.Message;
+
+
+@Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
 }
